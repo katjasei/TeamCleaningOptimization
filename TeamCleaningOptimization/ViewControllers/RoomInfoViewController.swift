96edd3fee481db5a972a22f30a9a31c1290e6727
oonarauhala <oona.e.rauhala@gmail.com>
2020-04-06 17:35:28 +0300

    Pass room data to report

--- TeamCleaningOptimization/ViewControllers/RoomInfoViewController.swift
+++ TeamCleaningOptimization/ViewControllers/RoomInfoViewController.swift
@@ -43,6 +43,15 @@ class RoomInfoViewController: UIViewController {
         }
     }
     
+    // Pass room data to report
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showReport" {
+            let destinationViewController = segue.destination as! ReportViewController
+            destinationViewController.roomNumb = self.getNumber
+            destinationViewController.time = self.time
+        }
+    }
+    
     //MARK: Actions
 
     @IBOutlet weak var startButton: RoundButton!
@@ -59,6 +68,7 @@ class RoomInfoViewController: UIViewController {
         timer.invalidate()
         print("Timer stopped")
         changeButtons()
+        self.performSegue(withIdentifier: "showReport", sender: "cleanedButton")
     }
     
     @IBOutlet weak var timeLabel: UILabel!
