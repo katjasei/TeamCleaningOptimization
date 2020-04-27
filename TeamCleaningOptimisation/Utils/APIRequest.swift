4d6a4b161900018497b1436aa40cc44aa963ecfb
katjasei <katjasei@metropolia.fi>
2020-04-27 14:05:06 +0300

    Xhange API request for heatmap data

--- TeamCleaningOptimisation/Utils/APIRequest.swift
+++ TeamCleaningOptimisation/Utils/APIRequest.swift
@@ -12,6 +12,7 @@ class APIRequest {
     private let endpoint = "https://cleaner-app-api.azurewebsites.net/api/hospital0"
     private let getRoomsString = "/rooms"
     private let getRoomString = "/room/"
+    private let getHeatmapString = "/heatmap"
     private let getReportString = "/reports"
     private let putStartCleaningString = "/startcleaning"
     private let putStopCleaningString = "/stopcleaning"
@@ -31,6 +32,11 @@ class APIRequest {
     func getReports(completion: @escaping (Result<Reports, Error>) -> Void) throws {
         guard let url = URL(string: endpoint+getReportString) else { return }
         doGETRequest(url: url, completion: completion)
+    }
+    
+    func getHeatmap(roomID: String, completionHandler: @escaping (_ contentLength: UInt64?) -> ()) throws {
+    guard let url = URL(string: endpoint+getRoomString+roomID+getHeatmapString) else { return }
+    doGETRequestHeatMap(for: url, completionHandler: completionHandler)
     }
     
     func putStartCleaning(roomID: String) throws {
@@ -135,5 +141,46 @@ class APIRequest {
         }
         task.resume()
     }
-}
+    
+    
+        // For heatmap GET request
+    
+        private func doGETRequestHeatMap(for url: URL, completionHandler: @escaping (_ contentLength: UInt64?) -> ()) {
+            
+            var request = URLRequest(url: url)
+            request.httpMethod = "GET"
+            //with authorization
+            request.setValue(API_KEY, forHTTPHeaderField: "Authorization")
+            //content-type
+            request.setValue("application/octet-stream", forHTTPHeaderField: "Content-Type")
+          
+            let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+                
+                // Check for errors
+                if let error = error {
+                    print("Error: \(error)")
+
+                    return
+                }
+                
+                // Read status code
+                if let response = response as? HTTPURLResponse {
+                    //print("GET request response status code: \(response.statusCode)")
+                    let contentLength = response.allHeaderFields["Content-Length"] as? String
+                    completionHandler(UInt64(contentLength!))
+                }
+                
+                // Read data
+                 // Convert HTTP Response Data to a simple String
+                   if let data = data, let dataString = String(data: data, encoding: .utf8) {
+                       print("Response data string:\n \(dataString)")
+                   }
+            }
+            task.resume()
+        }
+    }
+
+
+
+
 
