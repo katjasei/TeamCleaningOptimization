4c77f7340d86f78452ea94d2db8c81b53a80a789
katjasei <katjasei@metropolia.fi>
2020-05-03 23:08:02 +0300

    Add colorLUT function to create color table

--- TeamCleaningOptimisation/Extensions/UIColor.swift
+++ TeamCleaningOptimisation/Extensions/UIColor.swift
@@ -35,3 +35,14 @@ extension UIColor {
         return nil
     }
 }
+
+
+extension UIColor {
+    convenience init(red: Int, green: Int, blue: Int) {
+        let newRed = CGFloat(red)/255
+        let newGreen = CGFloat(green)/255
+        let newBlue = CGFloat(blue)/255
+        
+        self.init(red: newRed, green: newGreen, blue: newBlue, alpha: 1.0)
+    }
+}
