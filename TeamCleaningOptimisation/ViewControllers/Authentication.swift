2e6151d21369155e5a61ccd93534b14b5af50707
Artur Kulagin <38274948+KulaginArtur@users.noreply.github.com>
2020-05-06 12:26:46 +0300

    Delete Authentication.swift

--- TeamCleaningOptimisation/ViewControllers/Authentication.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Authentication.swift
-//  TeamCleaningOptimisation
-//
-//  Created by Artur Kulagin on 27.4.2020.
-//  Copyright © 2020 TeamCleaningOptimization. All rights reserved.
-//
-
-import Foundation
